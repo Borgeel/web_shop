49b97d5cbd7ea7e9e83868811c34d1ea7330f6a4
Irfan <irfancesir93@gmail.com>
2023-08-11 05:52:20 +0200

    reworked code to make it a bit nicer

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,21 @@
-const Header = () => {
-  return <div>Header</div>;
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = ({ user }) => {
+  return (
+    <header className="bg-blue-500 p-4 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-lg font-semibold">Web Shop</h1>
+        {user ? (
+          <div>Welcome, {user?.username}!</div>
+        ) : (
+          <Link to="/login" className="underline">
+            Login
+          </Link>
+        )}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
